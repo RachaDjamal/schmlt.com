be86bd0e353aebee1b2f134df05c548fdde8c96c
Rachadjamal <sacha.mallet1@gmail.com>
2024-02-07 12:58:11 +0100

    Add animations on index.html page

---
+++ js/indexAnimation.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const subtitles = [
+      "I'm a <span>tireless</span> ultra cyclist.",
+      "I'm <span>incorrigibly</span> curious.",
+      "I'm a <span>born</span> tinkerer.",
+      "I'm a <span>quick</span> learner.",
+      "I'm an <span>energetic</span> co-worker.",
+      "I'm a <span>former</span> entrepreneur.",
+      "I'm a <span>perpetual</span> maker.",
+      "I'm a <span>huge</span> 'The Office' fan.",
+      "I'm an <span>aspiring</span> web developer."
+  ];
+    let index = 0;
+
+    const changeHeading = () => {
+        const headingElement = document.getElementById('Subtitle');
+        anime({
+            targets: headingElement,
+            opacity: [1, 0],
+            duration: 1500,
+            easing: 'easeInOutQuad',
+            complete: () => {
+                index = (index + 1) % subtitles.length;
+                headingElement.innerHTML = subtitles[index];
+                anime({
+                    targets: headingElement,
+                    opacity: [0, 1],
+                    duration: 1500,
+                    easing: 'easeInOutQuad'
+                });
+            }
+        });
+    };
+
+    setInterval(changeHeading, 5000);
+});
